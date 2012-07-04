58a4ef848c109791c00bd9f6263867748c5fee8e
Gerard Ribas <gerard.ribas.canals@gmail.com>
2012-07-04 13:35:34 +0200

    Added new Spring - Dependency Injection - XML PoC

---
+++ spring-parent/spring-dependency-injection-annotation/src/main/java/es/gerardribas/example/spring/di/beans/impl/MyBeanImpl.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package es.gerardribas.example.spring.di.beans.impl;
+
+import es.gerardribas.example.spring.di.beans.MyBean;
+
+/**
+ * @author Gerard Ribas Canals (gerard.ribas.canals@gmail.com)
+ *
+ */
+public class MyBeanImpl implements MyBean {
+
+	@Override
+	public String getHelloWorld(String name) {
+		return "Hello " + name + ", this is a PoC Test!";
+	}
+
+}
