921f4400eb9d83da5c0f590d504a16f3f73929de
Gerard Ribas <gerard.ribas.canals@gmail.com>
2012-07-05 10:13:15 +0200

    Spring Vaadin PoC

---
+++ spring-parent/spring-vaadin/src/main/java/es/gerardribas/example/spring/vaadin/MyVaadinApplication.java
@@ -0,0 +1,63 @@
+package es.gerardribas.example.spring.vaadin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import com.vaadin.Application;
+import com.vaadin.data.Container;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.Window;
+
+import es.gerardribas.example.common.dao.ProductDao;
+import es.gerardribas.example.common.domain.Product;
+
+@SuppressWarnings("serial")
+@Configurable(preConstruction = true)
+public class MyVaadinApplication extends Application {
+
+	private Window window;
+	
+	@Autowired
+	private ProductDao productDao;
+	
+	private Table productTable;
+	
+	private Container productContainer;
+
+	@Override
+	public void init() {
+		window = new Window("My Vaadin Application");
+		setMainWindow(window);
+		
+		Button button = new Button("Show Products");
+		button.addListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				fillTable();
+				productTable.setVisible(true);
+			}
+		});
+		window.addComponent(new Label("Simple PoC for demostrate Spring - Vaadin configurable."));
+		window.addComponent(button);
+		window.addComponent(addTableToWindow());
+	}
+	
+	private void fillTable() {
+		if(productContainer == null) {
+			productContainer = new BeanItemContainer<Product>(Product.class, productDao.findAll(Product.class));
+			productTable.setContainerDataSource(productContainer);	
+		}
+		
+	}
+	
+	private Table addTableToWindow() {
+		productTable = new Table();
+		productTable.setVisible(false);
+		return productTable;
+	}
+	
+	
+}
